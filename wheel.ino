fb9f773b185e75130a2b8538a51106300336e56b
김채연 <codus3276@kakao.com>
2024-12-11 00:59:04 +0900

    Update: 바퀴 진로 방향 랜덤화

--- wheel.ino
+++ wheel.ino
@@ -1,62 +1,85 @@
-int ENA = 11;
-int ENB = 6;
-int IN1 = 10;
-int IN2 = 9;
-int IN3 = 8;
-int IN4 = 7;
+#include <Arduino.h>
 
-/*
- * ENA    IN1   IN2
- * HIGH   HIGH  LOW   모터 A 정회전(앞으로)
- * HIGH   LOW   HIGH  모터 A 역회전(뒤로)
- * HIGH   HIGH  HIGH  모터 A 급정지(브레이크)
- * HIGH   LOW   LOW   모터 A 급정지(브레이크)
- * LOW    X     X     모터 A 천천히정지(액셀 발뗌)
- */
+// 핀 정의
+#define ENA 6   // 왼쪽 바퀴 속도 제어 (PWM 핀)
+#define IN1 7   // 왼쪽 바퀴 제어 핀 1
+#define IN2 8   // 왼쪽 바퀴 제어 핀 2
+
+#define ENB 11  // 오른쪽 바퀴 속도 제어 (PWM 핀)
+#define IN3 9   // 오른쪽 바퀴 제어 핀 1
+#define IN4 10  // 오른쪽 바퀴 제어 핀 2
 
 void setup() {
+  // 핀 모드 설정
   pinMode(ENA, OUTPUT);
-  pinMode(ENB, OUTPUT);
   pinMode(IN1, OUTPUT);
   pinMode(IN2, OUTPUT);
+
+  pinMode(ENB, OUTPUT);
   pinMode(IN3, OUTPUT);
   pinMode(IN4, OUTPUT);
+
+  // 랜덤 시드 설정 (아날로그 핀의 노이즈로 시드 생성)
+  randomSeed(analogRead(0));
 }
 
 void loop() {
-  // 전진
+  int direction = random(1, 5); // 1 ~ 4까지의 랜덤 값 생성
+
+  switch (direction) {
+    case 1:
+      moveForward();
+      break;
+    case 2:
+      moveBackward();
+      break;
+    case 3:
+      turnLeft();
+      break;
+    case 4:
+      turnRight();
+      break;
+  }
+
+  delay(12000);
+}
+
+// 전진
+void moveForward() {
   digitalWrite(IN1, HIGH);
   digitalWrite(IN2, LOW);
   digitalWrite(IN3, HIGH);
   digitalWrite(IN4, LOW);
-  analogWrite(ENA, 255);
-  analogWrite(ENB, 255);
-  delay(2000);
+  analogWrite(ENA, 255); // 왼쪽 바퀴 속도
+  analogWrite(ENB, 255); // 오른쪽 바퀴 속도
+}
 
-  // 후진
+// 후진
+void moveBackward() {
   digitalWrite(IN1, LOW);
   digitalWrite(IN2, HIGH);
   digitalWrite(IN3, LOW);
   digitalWrite(IN4, HIGH);
-  analogWrite(ENA, 255);
-  analogWrite(ENB, 255);
-  delay(2000);
+  analogWrite(ENA, 255); // 왼쪽 바퀴 속도
+  analogWrite(ENB, 255); // 오른쪽 바퀴 속도
+}
 
-  // 좌회전
+// 좌회전
+void turnLeft() {
   digitalWrite(IN1, LOW);
-  digitalWrite(IN2, HIGH);
+  digitalWrite(IN2, LOW);
   digitalWrite(IN3, HIGH);
   digitalWrite(IN4, LOW);
-  analogWrite(ENA, 255);
-  analogWrite(ENB, 255);
-  delay(2000);
+  analogWrite(ENA, 0);   // 왼쪽 바퀴 정지
+  analogWrite(ENB, 255); // 오른쪽 바퀴 속도
+}
 
-  // 우회전
+// 우회전
+void turnRight() {
   digitalWrite(IN1, HIGH);
   digitalWrite(IN2, LOW);
   digitalWrite(IN3, LOW);
-  digitalWrite(IN4, HIGH);
-  analogWrite(ENA, 255);
-  analogWrite(ENB, 255);
-  delay(2000);
+  digitalWrite(IN4, LOW);
+  analogWrite(ENA, 255); // 왼쪽 바퀴 속도
+  analogWrite(ENB, 0);   // 오른쪽 바퀴 정지
 }
