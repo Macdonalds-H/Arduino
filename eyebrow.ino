9295bfa74fc4e515e285ece9af792fa826557d95
HannaJeong5 <147011759+HannaJeong5@users.noreply.github.com>
2024-12-11 17:06:56 +0900

    Update 변수 및 설명 수정

--- eyebrow.ino
+++ eyebrow.ino
@@ -5,18 +5,18 @@
 
 #include <Servo.h>
 
-Servo myServo1; // 첫 번째 서보 객체 생성
-Servo myServo2; // 두 번째 서보 객체 생성
-int angle1 = 0; // 첫 번째 서보의 현재 각도 초기화
-int angle2 = 180; // 두 번째 서보의 현재 각도 초기화 (최대 각도에서 시작)
+Servo myServo1; // 첫 번째 서보 객체 생성 (= 오른쪽 눈썹)
+Servo myServo2; // 두 번째 서보 객체 생성 (= 왼쪽 눈썹)
+int brow_R = 0; // 오른쪽 눈썹 초기화 각도
+int brow_L = 180; // 왼쪽 눈썹 초기화 각도
 
 void setup() {
-  myServo1.attach(9); // 첫 번째 서보 핀 설정 (핀 번호 9 사용)
-  myServo1.write(angle1); // 첫 번째 서보 초기 각도를 0도로 설정
-  myServo2.attach(7); // 두 번째 서보 핀 설정 (핀 번호 7 사용)
-  myServo2.write(angle2); // 두 번째 서보 초기 각도를 90도로 설정
+  myServo1.attach(9); // 서보1 핀 번호 9
+  myServo1.write(brow_R); // 오른쪽 눈썹(= 서보1) 초기 각도 설정
+  myServo2.attach(7); // 서보2 핀 번호 7
+  myServo2.write(brow_L); // 왼쪽 눈썹(서보2) 초기 각도 설정
   Serial.begin(9600); // 시리얼 통신 시작
-  Serial.println("서보 제어 프로그램 시작!");
+  Serial.println("서보 제어 프로그램 시작");
   Serial.println("1: 각도 증가, 2: 원래 각도로 복귀");
 }
 
@@ -24,34 +24,34 @@ void loop() {
   if (Serial.available()) {
     char input = Serial.read(); // 시리얼 모니터에서 입력값 읽기
     
-    if (input == '1') { // '1'을 입력받으면
-      if (angle1 < 50) { // 첫 번째 서보의 최대 각도는 50도
-        angle1 += 25;
-        if (angle1 > 50) {
-          angle1 = 50; // 첫 번째 서보 각도가 50도를 초과하지 않도록 제한
+    if (input == '1') { // '1'을 입력받으면 --> 알람이 울리면
+      if (brow_R < 50) { // 오른쪽 눈썹 최대 각도 50도
+        brow_R += 25;
+        if (brow_R > 50) {
+          brow_R = 50; // 50도를 초과하지 않도록 제한
         }
-        myServo1.write(angle1); // 첫 번째 서보 각도 설정
+        myServo1.write(brow_R); // 오른쪽 눈썹 각도 설정
         Serial.print("서보1 현재 각도: ");
-        Serial.println(angle1);
+        Serial.println(brow_R);
       } 
       
-      if (angle2 > 130) { // 두 번째 서보의 최소 각도는 130도
-        angle2 -= 25;
-        if (angle2 < 130) {
-          angle2 = 130; // 두 번째 서보 각도가 130도 미만이 되지 않도록 제한
+      if (brow_L > 130) { // 왼쪽 눈썹 최소 각도 130도
+        brow_L -= 25;
+        if (brow_L < 130) {
+          brow_L = 130; // 각도가 130도 미만이 되지 않도록 제한
         }
-        myServo2.write(angle2); // 두 번째 서보 각도 설정
+        myServo2.write(brow_L); // 왼쪽 눈썹 각도 설정
         Serial.print("서보2 현재 각도: ");
-        Serial.println(angle2);
+        Serial.println(brow_L);
       }
-    } else if (input == '2') { // '2'를 입력받으면
-      angle1 = 0;
-      myServo1.write(angle1); // 첫 번째 서보 각도를 0도로 설정
-      Serial.println("첫 번째 서보가 원래 각도로 복귀");
+    } else if (input == '2') { // '2'를 입력받으면 --> 알람을 끄면
+      brow_R = 0;
+      myServo1.write(brow_R); // 오른쪽 눈썹 각도를 0도로 설정
+      Serial.println("오른쪽 눈썹 원래 각도로 복귀");
       
-      angle2 = 180;
-      myServo2.write(angle2); // 두 번째 서보 각도를 180도로 설정
-      Serial.println("두 번째 서보가 원래 각도로 복귀");
+      brow_L = 180;
+      myServo2.write(brow_L); // 왼쪽 눈썹 각도를 180도로 설정
+      Serial.println("왼쪽 눈썹 원래 각도로 복귀");
     
     }
   }
